8f7c4b047ccfb982b389406f0f76738eefa4e265
SebastianGarces <gsgarces1@gmail.com>
2026-02-13 20:06:37 -0500

    removed localPattern from config

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,26 @@
-import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "vertical-church.t3.storage.dev",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'vertical-church.t3.storage.dev',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "registrations-production.s3.amazonaws.com",
-        pathname: "/uploads/**",
+        protocol: 'https',
+        hostname: 'registrations-production.s3.amazonaws.com',
+        pathname: '/uploads/**',
       },
       {
-        protocol: "https",
-        hostname: "img.youtube.com",
-        pathname: "/vi/**",
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
       },
     ],
-    localPatterns: [],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [48, 128, 256, 384],
     minimumCacheTTL: 2678400, // 31 days – most images are static and won't change
